687eca365264c44b1765f60bbf81d1a0857b737d
hecterbonha <untungsoandryanto@gmail.com>
2022-07-05 16:53:55 +0700

    fix(): add rate limitter

--- server/server_http.go
+++ server/server_http.go
@@ -8,12 +8,14 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func ServerHTTP() {
 	fmt.Println("Running HTTP on :4444")
 	app := fiber.New()
 	app.Use(cors.New())
+	app.Use(limiter.New())
 
 	router.SetupRoutes(app)
 	log.Fatal(app.Listen(":4444"))
